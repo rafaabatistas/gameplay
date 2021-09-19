04e0a82707f6c18a172c025d4dc2ffdd79d4e16c
Rafael <rafaabatistas@gmail.com>
2021-09-19 20:02:59 -0300

    CONFIG: plop

---
+++ generators/plopfile.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+module.exports = function (plop) {
+  plop.setGenerator('component', {
+    description: 'criando uma estrutura para um componente',
+    prompts: [
+      {
+        type: 'input',
+        name: 'type',
+        message: 'Qual o tipo do componente(atoms, molecules, organisms)?'
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Qual o nome do componente?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/ui/{{type}}/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'templates/index.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/ui/{{type}}/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/ui/{{type}}/{{pascalCase name}}/{{pascalCase name}}.styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
+      }
+    ]
+  });
+};

b0662828f843b389f844f1f84fd1a25ff5f978d6
Rafael <rafaabatistas@gmail.com>
2021-09-19 20:12:50 -0300

    FEAT: Adicionando o ThemeProvider no projeto

---
+++ src/styles/theme.ts
@@ -0,0 +1,50 @@
+export default {
+  border: {
+    radius: '8px'
+  },
+  font: {
+    family: {
+      primary: 'Rajdhani',
+      secundary: 'Inter'
+    },
+    thin: 300,
+    light: 400,
+    regular: 500,
+    medium: 600,
+    bold: 700,
+    sizes: {
+      small: '13px',
+      medium: '15px',
+      xmedium: '18px',
+      big: '24px',
+      large: '28px',
+      extra: '40px'
+    }
+  },
+  colors: {
+    primary: '#E61C44',
+    secondary: '#495BCC',
+    gradientBg1: '#0E1647',
+    gradientBg2: '#0A1033',
+    white: '#FFFFFF',
+    lightRed: '#F17575',
+    lightGray: '#DDE3F0',
+    gray: '#ABB1CC',
+    green: '#32BD50',
+    brown: '#D07622',
+    box1: '#1D2766',
+    box2: '#171F52',
+    stroke1: '#243189',
+    stroke2: '#1B2565',
+    line: '#991F36'
+  },
+  spacings: {
+    xxsmall: '8px',
+    xsmall: '16px',
+    small: '24px',
+    medium: '32px',
+    large: '40px',
+    xlarge: '48px',
+    xxlarge: '56px'
+  }
+};

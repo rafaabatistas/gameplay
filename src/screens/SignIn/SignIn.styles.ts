6536063935e34034199096555736c1a15e10a408
Rafael <rafaabatistas@gmail.com>
2021-09-19 20:09:15 -0300

    FEAT: Criando o componente <SignIn />

--- src/screens/SignIn/SignIn.styles.ts
+++ src/screens/SignIn/SignIn.styles.ts
@@ -1,14 +1,36 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #03133e;
   justify-content: center;
   align-items: center;
+  position: relative;
+`;
+
+export const Content = styled.View`
+  margin-top: -100px;
 `;
 
 export const Title = styled.Text`
-  font-size: 24px;
-  font-weight: 500;
-  color: palevioletred;
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.extra};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.lightGray};
+    margin-bottom: ${theme.spacings.xsmall};
+    text-align: center;
+  `}
+`;
+
+export const Description = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.colors.lightGray};
+    font-weight: ${theme.font.light};
+    margin-bottom: ${theme.spacings.small};
+    text-align: center;
+  `}
+`;
+
+export const Image = styled.Image`
+  width: 100%;
+  height: 360px;
 `;
--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
 import * as S from './SignIn.styles';
 
-export const SignIn = () => {
-  const bla = 'foo';
-  return (
-    <S.Container>
-      <S.Title>Hello World</S.Title>
-      <StatusBar style="auto" />
-    </S.Container>
-  );
-};
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import { Button } from '../../components/ui/atoms/Button/Button';
+
+import Illustration from '../../../assets/img/illustration.png';
+
+export const SignIn = () => (
+  <S.Container>
+    <LinearGradient colors={['#0E1647', '#0A1033']} style={{ height: '100%', width: '100%', position: 'absolute' }} />
+    <S.Image source={Illustration} resizeMode="stretch" />
+    <S.Content>
+      <S.Title>
+        Conecte-se {`\n`} e organize suas {`\n`} jogatinas
+      </S.Title>
+      <S.Description>Crie grupos para jogar seus games {`\n`} favoritos com seus amigos</S.Description>
+      <Button withIcon size="medium">
+        Entrar com Discord
+      </Button>
+    </S.Content>
+  </S.Container>
+);

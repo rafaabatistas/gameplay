6af8e5c573d31e1c85588fa67763e1002af1eaad
Rafael <rafaabatistas@gmail.com>
2021-09-26 20:04:30 -0300

    FEAT: Fazendo o uso do useFonts para carregamento dinamico da aplicação e adicionando <Container /> por volta de todas as screens

--- App.tsx
+++ App.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components/native';
 import theme from './src/styles/theme';
-import { StatusBar } from 'react-native';
 
-import { SignIn } from './src/screens/SignIn/SignIn';
+import { StatusBar } from 'react-native';
+import { Routes } from './src/routes';
+import { Container } from './src/components/ui/atoms/Container/Container';
 
 export default function App() {
-  return (
+  const [fontsLoaded] = useFonts({
+    Inter_500: require('./assets/fonts/inter-v3-latin-500.ttf'),
+    Inter_Regular: require('./assets/fonts/inter-v3-latin-regular.ttf'),
+    Rajdhani_500: require('./assets/fonts/rajdhani-v10-latin-500.ttf'),
+    Rajdhani_700: require('./assets/fonts/rajdhani-v10-latin-700.ttf'),
+    Rajdhani_Regular: require('./assets/fonts/rajdhani-v10-latin-regular.ttf')
+  });
+  return fontsLoaded ? (
     <ThemeProvider theme={theme}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      <SignIn />
+      <Container>
+        <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+        <Routes />
+      </Container>
     </ThemeProvider>
+  ) : (
+    <AppLoading />
   );
 }

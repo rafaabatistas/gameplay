ea7edad18df3cfa96d5dacbe5bfb64e247fd9f92
Rafael <rafaabatistas@gmail.com>
2021-09-26 20:05:36 -0300

    FEAT: Adicionando uma função de teste para a nevegação do projeto

--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,24 +1,31 @@
 import * as S from './SignIn.styles';
 
 import React from 'react';
-import { LinearGradient } from 'expo-linear-gradient';
+import { CommonActions, useNavigation } from '@react-navigation/native';
 
 import { Button } from '../../components/ui/atoms/Button/Button';
 
 import Illustration from '../../../assets/img/illustration.png';
 
-export const SignIn = () => (
-  <S.Container>
-    <LinearGradient colors={['#0E1647', '#0A1033']} style={{ height: '100%', width: '100%', position: 'absolute' }} />
-    <S.Image source={Illustration} resizeMode="stretch" />
-    <S.Content>
-      <S.Title>
-        Conecte-se {`\n`} e organize suas {`\n`} jogatinas
-      </S.Title>
-      <S.Description>Crie grupos para jogar seus games {`\n`} favoritos com seus amigos</S.Description>
-      <Button withIcon size="medium">
-        Entrar com Discord
-      </Button>
-    </S.Content>
-  </S.Container>
-);
+export const SignIn = () => {
+  const navigation = useNavigation();
+
+  const handleSignIn = () => {
+    navigation.dispatch(CommonActions.navigate({ name: 'Home' }));
+  };
+
+  return (
+    <S.Container>
+      <S.Image source={Illustration} resizeMode="stretch" />
+      <S.Content>
+        <S.Title>
+          Conecte-se {'\n'} e organize suas {'\n'} jogatinas
+        </S.Title>
+        <S.Description>Crie grupos para jogar seus games {'\n'} favoritos com seus amigos</S.Description>
+        <Button withIcon size="medium" handle={() => handleSignIn()}>
+          Entrar com Discord
+        </Button>
+      </S.Content>
+    </S.Container>
+  );
+};

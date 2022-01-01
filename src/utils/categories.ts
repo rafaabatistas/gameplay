8d7362e6444a4e1beeff28f86da04441476da80a
Rafael <rafaabatistas@gmail.com>
2022-01-01 19:19:23 -0300

    FEAT: Criando arquivo de mock para as categorias

---
+++ src/utils/categories.ts
@@ -0,0 +1,11 @@
+import RankedIcon from '../../assets/svg/ranked.svg';
+import DuelIcon from '../../assets/svg/duel.svg';
+import FunIcon from '../../assets/svg/fun.svg';
+import TrainingIcon from '../../assets/svg/training.svg';
+
+export const categories = [
+  { id: '1', title: 'Ranqueada', icon: RankedIcon },
+  { id: '2', title: 'Duelo 1x1', icon: DuelIcon },
+  { id: '3', title: 'Diversão', icon: FunIcon },
+  { id: '4', title: 'Treino', icon: TrainingIcon }
+];

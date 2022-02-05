5aa25049a8e942b7638100a7f2ccae5bfcfa1ca1
Rafael Batista <rafaabatistas@gmail.com>
2022-02-05 15:08:31 -0300

    FEAT: Criando uma rota para o Storybook

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { Home } from '../screens/Home/Home';
 import { SignIn } from '../screens/SignIn/SignIn';
+import { Storybook } from '../screens/Storybook/Storybook';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -18,5 +19,6 @@ export const AuthRoutes = () => (
   >
     <Screen name="SignIn" component={SignIn} />
     <Screen name="Home" component={Home} />
+    <Screen name="Storybook" component={Storybook} />
   </Navigator>
 );

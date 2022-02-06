72da4514d77d4ee0e4c5eee4a325c3688436012f
Rafael Batista <rafaabatistas@gmail.com>
2022-02-05 22:49:52 -0300

    FEAT: Criando o arquivo mock para a listagem dos appointments

---
+++ src/screens/Home/home.mock.ts
@@ -0,0 +1,40 @@
+const appointments = [
+  {
+    id: '1',
+    guild: {
+      id: '1',
+      name: 'Lendários',
+      icon: null,
+      owner: true
+    },
+    category: '1',
+    date: '22/06 às 20:40h',
+    description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+  },
+  {
+    id: '2',
+    guild: {
+      id: '1',
+      name: 'Lendários',
+      icon: null,
+      owner: true
+    },
+    category: '1',
+    date: '22/06 às 20:40h',
+    description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+  },
+  {
+    id: '3',
+    guild: {
+      id: '1',
+      name: 'Lendários',
+      icon: null,
+      owner: true
+    },
+    category: '1',
+    date: '22/06 às 20:40h',
+    description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+  }
+];
+
+export default appointments;

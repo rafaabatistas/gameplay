ba6b3f9d6a13fb14c46d61a9c20b8ffbc255c9cc
Rafael Batista <rafaabatistas@gmail.com>
2022-02-09 20:39:45 -0300

    STYLES: Alterando o largura do <ListDivider />

--- src/components/ui/atoms/ListDivider/ListDivider.tsx
+++ src/components/ui/atoms/ListDivider/ListDivider.tsx
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components/native';
 
 export const ListDivider = styled.View`
   ${({ theme }) => css`
-    width: 78%;
+    width: 74%;
     height: 1px;
     background-color: ${theme.colors.box1};
     margin-top: 2px;

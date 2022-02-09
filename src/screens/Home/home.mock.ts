37508cddb62bb449c076e91907541200720ad7fb
Rafael Batista <rafaabatistas@gmail.com>
2022-02-09 20:48:51 -0300

    CHORE: Deixando o arquivo de mock da listagem da <Home /> mais interessante

--- src/screens/Home/home.mock.ts
+++ src/screens/Home/home.mock.ts
@@ -4,7 +4,7 @@ const appointments = [
     guild: {
       id: '1',
       name: 'Lendários',
-      icon: null,
+      icon: 'https://pentagram-production.imgix.net/cc7fa9e7-bf44-4438-a132-6df2b9664660/EMO_LOL_02.jpg?rect=0%2C0%2C1440%2C1512&w=640&crop=1&fm=jpg&q=70&auto=format&fit=crop&h=672',
       owner: true
     },
     category: '1',
@@ -15,8 +15,8 @@ const appointments = [
     id: '2',
     guild: {
       id: '1',
-      name: 'Lendários',
-      icon: null,
+      name: 'Rumo ao topo',
+      icon: 'https://img1.gratispng.com/20180903/bj/kisspng-counter-strike-global-offensive-logo-stencil-art-counter-strike-global-offensive-blanket-www-imgarc-5b8de089921b09.7048778715360247135985.jpg',
       owner: true
     },
     category: '1',
@@ -27,8 +27,32 @@ const appointments = [
     id: '3',
     guild: {
       id: '1',
-      name: 'Lendários',
-      icon: null,
+      name: 'Bora queimar tudo',
+      icon: 'https://nosnerds.com.br/wp-content/uploads/2019/02/apexlegends-capa.jpg',
+      owner: true
+    },
+    category: '1',
+    date: '22/06 às 20:40h',
+    description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+  },
+  {
+    id: '4',
+    guild: {
+      id: '1',
+      name: 'Yeah, Boy',
+      icon: 'https://img.ibxk.com.br/2018/05/03/red-dead-03201115279005.jpg',
+      owner: true
+    },
+    category: '1',
+    date: '22/06 às 20:40h',
+    description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+  },
+  {
+    id: '5',
+    guild: {
+      id: '1',
+      name: 'Rolezão Monstro',
+      icon: 'http://s2.glbimg.com/Sykd3zf6hdvVGv41G4Y6rvhZg38=/290x355/s2.glbimg.com/vzNXMUGlexLYtTxSuxdX47CTJRLLkRxw_Iug2BPwenNIoz-HdGixxa_8qOZvMp3w/s.glbimg.com/jo/g1/f/original/2013/04/02/ogt0rx5z.jpg',
       owner: true
     },
     category: '1',

fe030345d4d310879c27234f72b0cd9fd1452d87
Rafael Batista <rafaabatistas@gmail.com>
2022-02-09 20:38:34 -0300

    FIX: Validando url dentro do componente <GuildIcon />

--- src/components/ui/atoms/GuildIcon/GuildIcon.tsx
+++ src/components/ui/atoms/GuildIcon/GuildIcon.tsx
@@ -2,11 +2,13 @@ import * as S from './GuildIcon.styles';
 
 import React from 'react';
 
+import DiscodChild from '../../../../../assets/img/discord-child.png';
+
 export type GuildIconProps = {
-  uri: string;
+  uri: string | null;
   marginRight?: boolean;
 };
 
 export const GuildIcon = ({ uri, marginRight = true }: GuildIconProps) => (
-  <S.Wrapper marginRight={marginRight} source={{ uri }} resizeMode="cover" />
+  <S.Wrapper marginRight={marginRight} source={uri !== null ? { uri } : DiscodChild} resizeMode="cover" />
 );

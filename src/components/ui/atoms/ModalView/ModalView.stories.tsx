39105500f19c16978fe14b1cc332cdb030a6189a
Rafael Batista <rafaabatistas@gmail.com>
2022-02-10 09:40:58 -0300

    FIX: Melhorando o stories do <ModalView />

--- src/components/ui/atoms/ModalView/ModalView.stories.tsx
+++ src/components/ui/atoms/ModalView/ModalView.stories.tsx
@@ -1,6 +1,6 @@
 import { storiesOf } from '@storybook/react-native';
 import React, { useState } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { Button } from '../Button/Button';
 import { ModalView } from './ModalView';
 
@@ -9,14 +9,18 @@ const ComponentModal = () => {
 
   return (
     <>
-      <Button handle={() => setOpenModal(true)} size="medium">
-        Abrir
-      </Button>
-      <ModalView visible={openModal}>
-        <Text style={{ color: '#fff' }}>Hello World!</Text>
-        <Button handle={() => setOpenModal(false)} size="medium">
-          Fechar
+      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <Button handle={() => setOpenModal(true)} size="medium">
+          Abrir
         </Button>
+      </View>
+      <ModalView visible={openModal} closeModal={() => setOpenModal(false)}>
+        <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{ color: '#fff', marginBottom: 25 }}>Hello World!</Text>
+          <Button handle={() => setOpenModal(false)} size="medium">
+            Fechar
+          </Button>
+        </View>
       </ModalView>
     </>
   );

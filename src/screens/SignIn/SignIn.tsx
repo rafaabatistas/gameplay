388ee8b76707a2559c3a86dfe17cca60e81dd89f
Rafael Batista <rafaabatistas@gmail.com>
2022-02-12 21:26:09 -0300

    FEAT: Adicionando um loading na tela de <SignIn /> enquanto o login está sendo feito

--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,18 +1,26 @@
 import * as S from './SignIn.styles';
 
 import React from 'react';
-import { CommonActions, useNavigation } from '@react-navigation/native';
+import { ActivityIndicator } from 'react-native';
+
+import { useAuth } from '../../hooks/auth';
 
 import { Button } from '../../components/ui/atoms/Button/Button';
 import { Container } from '../../components/ui/atoms/Container/Container';
 
+import theme from '../../styles/theme';
+
 import Illustration from '../../../assets/img/illustration.png';
 
 export const SignIn = () => {
-  const navigation = useNavigation();
+  const { loading, signIn } = useAuth();
 
-  const handleSignIn = () => {
-    navigation.dispatch(CommonActions.navigate({ name: 'Home' }));
+  const handleSignIn = async () => {
+    try {
+      await signIn();
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -24,9 +32,13 @@ export const SignIn = () => {
             Conecte-se {'\n'} e organize suas {'\n'} jogatinas
           </S.Title>
           <S.Description>Crie grupos para jogar seus games {'\n'} favoritos com seus amigos</S.Description>
-          <Button withIcon size="medium" handle={() => handleSignIn()}>
-            Entrar com Discord
-          </Button>
+          {loading ? (
+            <ActivityIndicator color={theme.colors.primary} size={36} />
+          ) : (
+            <Button withIcon size="medium" handle={() => handleSignIn()}>
+              Entrar com Discord
+            </Button>
+          )}
         </S.Content>
       </S.Wrapper>
     </Container>

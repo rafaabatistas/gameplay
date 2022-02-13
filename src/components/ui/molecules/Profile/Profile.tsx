53c7e89b42a9b3a43ab93feb20664e78d99b4977
Rafael Batista <rafaabatistas@gmail.com>
2022-02-12 21:22:50 -0300

    FEAT: Utilizando as informações do user do Contexto no <Profile />

--- src/components/ui/molecules/Profile/Profile.tsx
+++ src/components/ui/molecules/Profile/Profile.tsx
@@ -2,17 +2,23 @@ import * as S from './Profile.styles';
 
 import React from 'react';
 
+import { useAuth } from '../../../../hooks/auth';
+
 import { Avatar } from '../../atoms/Avatar/Avatar';
 
-export const Profile = () => (
-  <S.Container>
-    <Avatar image="https://github.com/rafaabatistas.png" />
-    <S.Box>
-      <S.User>
-        <S.Greeting>Olá, </S.Greeting>
-        <S.UserName>Rafael</S.UserName>
-      </S.User>
-      <S.Message>Hoje é dia de vitória</S.Message>
-    </S.Box>
-  </S.Container>
-);
+export const Profile = () => {
+  const { user } = useAuth();
+
+  return (
+    <S.Container>
+      <Avatar image={user.avatar} />
+      <S.Box>
+        <S.User>
+          <S.Greeting>Olá, </S.Greeting>
+          <S.UserName>{user.firstName}</S.UserName>
+        </S.User>
+        <S.Message>Hoje é dia de vitória</S.Message>
+      </S.Box>
+    </S.Container>
+  );
+};

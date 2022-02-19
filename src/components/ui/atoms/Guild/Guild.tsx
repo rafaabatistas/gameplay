1e95b097334704591b701d1f2092e2bd88a4fae2
Rafael Batista <rafaabatistas@gmail.com>
2022-02-19 20:50:32 -0300

    FEAT: Criando lógica para busca das imagens dos servidores do discord no component <Guild />

--- src/components/ui/atoms/Guild/Guild.tsx
+++ src/components/ui/atoms/Guild/Guild.tsx
@@ -8,10 +8,12 @@ import { GuildIcon } from '../GuildIcon/GuildIcon';
 
 import theme from '../../../../styles/theme';
 
+const { CDN_IMAGE } = process.env;
+
 export type DataGuildProps = {
   id: string;
   name: string;
-  subtitle: string;
+  subtitle?: string;
   icon: string | null;
   numberMembers: number;
 };
@@ -21,15 +23,19 @@ export type GuildProps = TouchableOpacityProps & {
   handleGuildSelect: (guild: DataGuildProps) => void;
 };
 
-export const Guild = ({ data, handleGuildSelect, ...rest }: GuildProps) => (
-  <S.Wrapper {...rest} activeOpacity={0.7} onPress={() => handleGuildSelect(data)}>
-    <GuildIcon uri={data.icon} withBorder={false} />
-    <S.Content>
-      <View>
-        <S.Title>{data.name}</S.Title>
-        <S.Subtitle>{data.subtitle}</S.Subtitle>
-      </View>
-      <Feather name="chevron-right" color={theme.colors.lightGray} size={24} />
-    </S.Content>
-  </S.Wrapper>
-);
+export const Guild = ({ data, handleGuildSelect, ...rest }: GuildProps) => {
+  const uri = data.icon !== null ? `${CDN_IMAGE}/icons/${data.id}/${data.icon}.png` : null;
+
+  return (
+    <S.Wrapper {...rest} activeOpacity={0.7} onPress={() => handleGuildSelect(data)}>
+      <GuildIcon uri={uri} withBorder={false} />
+      <S.Content>
+        <View>
+          <S.Title>{data.name}</S.Title>
+          <S.Subtitle>{data.subtitle}</S.Subtitle>
+        </View>
+        <Feather name="chevron-right" color={theme.colors.lightGray} size={24} />
+      </S.Content>
+    </S.Wrapper>
+  );
+};

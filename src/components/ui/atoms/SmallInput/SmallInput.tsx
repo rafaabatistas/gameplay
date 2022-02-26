c9920e1b3e0aae6ecf88274a56af5f6b957ecca9
Rafael Batista <rafaabatistas@gmail.com>
2022-02-26 18:12:23 -0300

    FEAT: Animando o componente <SmallInput /> quando houver algum erro na inserção dos dados

--- src/components/ui/atoms/SmallInput/SmallInput.tsx
+++ src/components/ui/atoms/SmallInput/SmallInput.tsx
@@ -4,16 +4,25 @@ import React from 'react';
 
 import { TextInputProps } from 'react-native';
 
+import { MotiView } from 'moti';
+
 export type SmallInputProps = TextInputProps & {
   error?: boolean;
 };
 
 export const SmallInput = ({ error, ...rest }: SmallInputProps) => (
-  <S.BackgroundInput>
-    <S.BackgroundError error={error}>
-      <S.ContentInput>
-        <S.Input<React.ElementType<TextInputProps>> keyboardType="numeric" {...rest} />
-      </S.ContentInput>
-    </S.BackgroundError>
-  </S.BackgroundInput>
+  <MotiView
+    from={{ translateX: 0 }}
+    animate={{ translateX: error ? [2, 0, -2, 0] : 0 }}
+    exit={{ translateX: 0 }}
+    transition={{ type: 'timing', duration: 50 }}
+  >
+    <S.BackgroundInput>
+      <S.BackgroundError error={error}>
+        <S.ContentInput>
+          <S.Input<React.ElementType<TextInputProps>> keyboardType="numeric" {...rest} />
+        </S.ContentInput>
+      </S.BackgroundError>
+    </S.BackgroundInput>
+  </MotiView>
 );

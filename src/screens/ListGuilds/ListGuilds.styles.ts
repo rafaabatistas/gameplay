1466b1dee30668e6ca84ab4cc026972a0a9f392e
Rafael Batista <rafaabatistas@gmail.com>
2022-02-27 18:29:53 -0300

    FEAT: Buscando todos os servidores do user no componente <ListGuilds />

--- src/screens/ListGuilds/ListGuilds.styles.ts
+++ src/screens/ListGuilds/ListGuilds.styles.ts
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components/native';
 
 export const Wrapper = styled.View`
   ${({ theme }) => css`
+    flex: 1;
     align-items: center;
     padding-top: ${theme.spacings.small};
   `}
@@ -10,4 +11,12 @@ export const Wrapper = styled.View`
 export const ListGuilds = styled.FlatList`
   width: 100%;
   margin-bottom: 24px;
+`;
+
+export const BoxAnimation = styled.View`
+  flex: 1;
+  width: 180px;
+  height: 180px;
+  align-items: center;
+  justify-content: center;
 `;
--- src/screens/ListGuilds/ListGuilds.tsx
+++ src/screens/ListGuilds/ListGuilds.tsx
@@ -1,23 +1,59 @@
 import * as S from './ListGuilds.styles';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatListProps } from 'react-native';
+import LottieView from 'lottie-react-native';
 
-import { Guild, DataGuildProps, GuildProps } from '../../components/ui/atoms/Guild/Guild';
 import { ListDivider } from '../../components/ui/atoms/ListDivider/ListDivider';
+import { LoadingSpinner } from '../../components/ui/atoms/LoadingSpinner/LoadingSpinner';
+import { Guild, DataGuildProps, GuildProps } from '../../components/ui/atoms/Guild/Guild';
 
-import guilds from './listGuilds.mock';
+import api from '../../services/api';
+
+import animation from '../../../assets/json/error.json';
 
 export type ListGuildsProps = Pick<GuildProps, 'handleGuildSelect'>;
 
-export const ListGuilds = ({ handleGuildSelect }: ListGuildsProps) => (
-  <S.Wrapper>
-    <S.ListGuilds<React.ElementType<FlatListProps<any>>>
-      data={guilds}
-      keyExtractor={(item: DataGuildProps) => item.id}
-      renderItem={({ item }: { item: DataGuildProps }) => <Guild data={item} handleGuildSelect={handleGuildSelect} />}
-      showsVerticalScrollIndicator={false}
-      ItemSeparatorComponent={() => <ListDivider />}
-    />
-  </S.Wrapper>
-);
+export const ListGuilds = ({ handleGuildSelect }: ListGuildsProps) => {
+  const [guilds, setGuilds] = useState<GuildProps[]>([]);
+  const [animationError, setAnimationError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const fetchGuilds = async () => {
+    try {
+      const res = await api.get('/users/@me/guilds');
+      setGuilds(res.data);
+    } catch (err) {
+      setAnimationError(true);
+      throw new Error('Erro ao buscar os servidores');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchGuilds();
+  }, []);
+
+  return (
+    <S.Wrapper>
+      {loading ? (
+        <LoadingSpinner />
+      ) : animationError ? (
+        <S.BoxAnimation>
+          <LottieView source={animation} autoPlay loop={false} resizeMode="contain" />
+        </S.BoxAnimation>
+      ) : (
+        <S.ListGuilds<React.ElementType<FlatListProps<any>>>
+          data={guilds}
+          keyExtractor={(item: DataGuildProps) => item.id}
+          renderItem={({ item }: { item: DataGuildProps }) => (
+            <Guild data={item} handleGuildSelect={handleGuildSelect} />
+          )}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <ListDivider />}
+        />
+      )}
+    </S.Wrapper>
+  );
+};

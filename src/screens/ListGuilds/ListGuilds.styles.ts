c962902eae86c5b6166361740870c0c25925f289
Rafael Batista <rafaabatistas@gmail.com>
2022-03-01 19:40:23 -0300

    REFACTOR: Utilizando o hook useFetch dentro do <ListGuilds />

--- src/screens/ListGuilds/ListGuilds.styles.ts
+++ src/screens/ListGuilds/ListGuilds.styles.ts
@@ -1,10 +1,31 @@
 import styled, { css } from 'styled-components/native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export const Wrapper = styled.View`
   ${({ theme }) => css`
     flex: 1;
     align-items: center;
     padding-top: ${theme.spacings.small};
+  `}
+`;
+
+export const Container = styled(LinearGradient).attrs({
+  colors: ['#0E1647', '#0A1033'],
+  start: { x: 0, y: 0 },
+  end: { x: 0, y: 1 }
+})`
+  flex: 1;
+  margin-top: 110px;
+`;
+
+export const Bar = styled.View`
+  ${({ theme }) => css`
+    width: 39px;
+    height: 2px;
+    border-radius: 2px;
+    background-color: ${theme.colors.secondary};
+    align-self: center;
+    margin-top: 13px;
   `}
 `;
 
--- src/screens/ListGuilds/ListGuilds.tsx
+++ src/screens/ListGuilds/ListGuilds.tsx
@@ -1,6 +1,6 @@
 import * as S from './ListGuilds.styles';
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { FlatListProps } from 'react-native';
 import LottieView from 'lottie-react-native';
 
@@ -8,55 +8,39 @@ import { ListDivider } from '../../components/ui/atoms/ListDivider/ListDivider';
 import { LoadingSpinner } from '../../components/ui/atoms/LoadingSpinner/LoadingSpinner';
 import { Guild, DataGuildProps, GuildProps } from '../../components/ui/atoms/Guild/Guild';
 
-import api from '../../services/api';
-
 import Animation from '../../../assets/json/error.json';
+import { useFetch } from '../../hooks/useFetch';
 
 export type ListGuildsProps = Pick<GuildProps, 'handleGuildSelect'>;
 
 export const ListGuilds = ({ handleGuildSelect }: ListGuildsProps) => {
-  const [guilds, setGuilds] = useState<GuildProps[]>([]);
-  const [animationError, setAnimationError] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-  const fetchGuilds = async () => {
-    try {
-      const res = await api.get('/users/@me/guilds');
-      setGuilds(res.data);
-    } catch (err) {
-      setAnimationError(true);
-      throw new Error('Erro ao buscar os servidores');
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchGuilds();
-  }, []);
+  const { data: guilds, error, isLoading } = useFetch<GuildProps[]>('/users/@me/guilds');
 
   return (
-    <S.Wrapper>
-      {loading ? (
-        <LoadingSpinner />
-      ) : animationError ? (
-        <S.BoxAnimation>
-          <S.Animation>
-            <LottieView source={Animation} autoPlay loop={false} resizeMode="contain" />
-          </S.Animation>
-          <S.Description>Erro ao listar os servidores. Tente novamente mais tarde.</S.Description>
-        </S.BoxAnimation>
-      ) : (
-        <S.ListGuilds<React.ElementType<FlatListProps<any>>>
-          data={guilds}
-          keyExtractor={(item: DataGuildProps) => item.id}
-          renderItem={({ item }: { item: DataGuildProps }) => (
-            <Guild data={item} handleGuildSelect={handleGuildSelect} />
-          )}
-          showsVerticalScrollIndicator={false}
-          ItemSeparatorComponent={() => <ListDivider />}
-        />
-      )}
-    </S.Wrapper>
+    <S.Container>
+      <S.Bar />
+      <S.Wrapper>
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : error ? (
+          <S.BoxAnimation>
+            <S.Animation>
+              <LottieView source={Animation} autoPlay loop={false} resizeMode="contain" />
+            </S.Animation>
+            <S.Description>Erro ao listar os servidores. Tente novamente mais tarde.</S.Description>
+          </S.BoxAnimation>
+        ) : (
+          <S.ListGuilds<React.ElementType<FlatListProps<any>>>
+            data={guilds}
+            keyExtractor={(item: DataGuildProps) => item.id}
+            renderItem={({ item }: { item: DataGuildProps }) => (
+              <Guild data={item} handleGuildSelect={handleGuildSelect} />
+            )}
+            showsVerticalScrollIndicator={false}
+            ItemSeparatorComponent={() => <ListDivider />}
+          />
+        )}
+      </S.Wrapper>
+    </S.Container>
   );
 };

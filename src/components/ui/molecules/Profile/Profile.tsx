d9a20552fde2a1ee400ba45d5c0e545e3050a1be
Rafael Batista <rafaabatistas@gmail.com>
2022-03-01 19:38:14 -0300

    FEAT: Adicionando a chamada do modal de logout no <Profile />

--- src/components/ui/molecules/Profile/Profile.tsx
+++ src/components/ui/molecules/Profile/Profile.tsx
@@ -1,17 +1,29 @@
 import * as S from './Profile.styles';
 
-import React from 'react';
+import React, { useState } from 'react';
+import { RectButton } from 'react-native-gesture-handler';
 
 import { useAuth } from '../../../../hooks/auth';
 
+import { SignOut } from '../SignOut/SignOut';
 import { Avatar } from '../../atoms/Avatar/Avatar';
+import { ModalView } from '../../atoms/ModalView/ModalView';
 
 export const Profile = () => {
-  const { user } = useAuth();
+  const [openGuildModal, setOpenGuildModal] = useState(false);
+
+  const { user, signOut } = useAuth();
+
+  const handleSignOut = () => {
+    setOpenGuildModal(false);
+    signOut();
+  };
 
   return (
     <S.Container>
-      <Avatar image={user.avatar} />
+      <RectButton onPress={() => setOpenGuildModal(true)}>
+        <Avatar image={user.avatar} />
+      </RectButton>
       <S.Box>
         <S.User>
           <S.Greeting>Olá, </S.Greeting>
@@ -19,6 +31,9 @@ export const Profile = () => {
         </S.User>
         <S.Message>Hoje é dia de vitória</S.Message>
       </S.Box>
+      <ModalView visible={openGuildModal} closeModal={() => setOpenGuildModal(false)}>
+        <SignOut handleCancel={setOpenGuildModal} handleSignOut={handleSignOut} />
+      </ModalView>
     </S.Container>
   );
 };

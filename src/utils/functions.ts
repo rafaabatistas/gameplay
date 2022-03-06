cd0fc841f5d847480aa2b61de91e64cca0d09a2d
Rafael Batista <rafaabatistas@gmail.com>
2022-03-05 23:46:12 -0300

    FEAT: Criando função que pede permissão das notificações e retorna o token

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native';
+import Constants from 'expo-constants';
 import * as Notifications from 'expo-notifications';
 
 const { CDN_IMAGE } = process.env;
@@ -39,4 +41,36 @@ export const schedulePushNotification = async (title: string, body: string, data
     },
     trigger: date
   });
+};
+
+export const registerForPushNotificationsAsync = async () => {
+  let token;
+
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#0A1033'
+    });
+  }
+
+  return token;
 };

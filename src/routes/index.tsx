31113260b5fd2d3fd1c0ef4e527b510db80db1d0
Rafael Batista <rafaabatistas@gmail.com>
2022-03-06 15:22:29 -0300

    FEAT: Registrando toda a navegação do user no Analytics

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useRef } from 'react';
+import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
+import * as Analytics from 'expo-firebase-analytics';
 
 import { useAuth } from '../hooks/auth';
 import { AppRoutes } from './app.routes';
@@ -8,6 +9,31 @@ import { SignIn } from '../screens/SignIn/SignIn';
 
 export const Routes = () => {
   const { user } = useAuth();
+  const navigationRef = useNavigationContainerRef();
+  const routeNameRef = useRef<any>();
 
-  return <NavigationContainer>{user.id ? <AppRoutes /> : <SignIn />}</NavigationContainer>;
+  return (
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        routeNameRef.current = navigationRef.current?.getCurrentRoute()?.name;
+      }}
+      onStateChange={async () => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef.current?.getCurrentRoute()?.name;
+        console.log(previousRouteName, currentRouteName, 'bla');
+        if (previousRouteName !== currentRouteName) {
+          await Analytics.logEvent(`${currentRouteName}`, {
+            screen: `${currentRouteName}`,
+            user: user.id ?? 'Ainda não está logado',
+            purpose: `Navegando entre as rotas, de ${previousRouteName} para ${currentRouteName}`
+          });
+        }
+
+        routeNameRef.current = currentRouteName;
+      }}
+    >
+      {user.id ? <AppRoutes /> : <SignIn />}
+    </NavigationContainer>
+  );
 };
